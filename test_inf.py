44604c6b1093ecf42707b14acc51d4fdd71daa76
da03 <dengyuntian@gmail.com>
2022-09-19 15:24:55 -0400

    .

--- test_inf.py
+++ test_inf.py
@@ -20,13 +20,12 @@ from accelerate import notebook_launcher
 from accelerate import Accelerator
 from PIL import Image
 #torch.backends.cuda.matmul.allow_tf32=True
-from tqdm.auto import tqdm
-batch_size = 32
-#batch_size = 16
+import tqdm
+#batch_size = 32
+batch_size = 16
 @dataclass
 class TrainingConfig:
     image_size = (64, 320)  # the generated image resolution
-    train_batch_size = batch_size
     eval_batch_size = batch_size  # how many images to sample during evaluation
     num_epochs = 50
     gradient_accumulation_steps = 1
@@ -36,7 +35,7 @@ class TrainingConfig:
     save_image_epochs = 10
     save_model_epochs = 30
     mixed_precision = 'no'  # `no` for float32, `fp16` for automatic mixed precision
-    output_dir = 'ddpm-butterflies-128'  # the model namy locally and on the HF Hub
+    output_dir = 'images_rendered'  # the model namy locally and on the HF Hub
 
     push_to_hub = False  # whether to upload the saved model to the HF Hub
     hub_private_repo = False  
@@ -55,12 +54,18 @@ dataset = concatenate_datasets([dataset1, dataset2, dataset3])
 #dataset = concatenate_datasets([dataset1, dataset2])
 dataset = dataset.shuffle(seed=42)
 
+
+# for actual testing
+config.dataset_name = "yuntian-deng/im2latex-100k"
+dataset = load_dataset(config.dataset_name, split="test")
+dataset = dataset.shuffle(seed=42)
+
 #config.dataset_name = "yuntian-deng/im2latex-100k-raw"
 #dataset = load_dataset(config.dataset_name, split="val")
 
 model_type = "EleutherAI/gpt-neo-125M"
 
-#tokenizer = AutoTokenizer.from_pretrained("t5-small")
+#tokenizer = AutoTokenizer.from_pretrained("text-small")
 tokenizer = AutoTokenizer.from_pretrained(model_type, max_length=1024)
 #tokenizer.add_special_tokens({'pad_token': 'Ġgazed'})
 #import pdb; pdb.set_trace()
@@ -76,15 +81,15 @@ preprocess = transforms.Compose(
 )
 
 def preprocess_formula(formula):
-  formula1 = 'a + b = c'
-  formula2 = 'a+b=c'
-  formula3 = r'd s _ { 1 1 } ^ { 2 } = d x ^ { + } d x ^ { - } + l _ { p } ^ { 9 } \frac { p _ { - } } { r ^ { 7 } } \delta ( x ^ { - } ) d x ^ { - } d x ^ { - }'
-  formula4 = '1 + 2 = 3'
-  formula5 = r'\cat \elephant \frog'
-  formula6 = r'1 + \cat - 2 + \elephant = \frog'
-  formula7 = r'a + b \cat - c + \elephant = \frog'
-  formula8 = r'd s _ { 1 1 } ^ { 2 } \frog = \crab d x ^ { + } d x ^ { - } + l _ { p } ^ { 9 } \frac { p _ { - } } { r ^ { 7 } } \delta ( x ^ { - } ) d x ^ { - } d x ^ { - }'
-  import random
+  #formula1 = 'a + b = c'
+  #formula2 = 'a+b=c'
+  #formula3 = r'd s _ { 1 1 } ^ { 2 } = d x ^ { + } d x ^ { - } + l _ { p } ^ { 9 } \frac { p _ { - } } { r ^ { 7 } } \delta ( x ^ { - } ) d x ^ { - } d x ^ { - }'
+  #formula4 = '1 + 2 = 3'
+  #formula5 = r'\cat \elephant \frog'
+  #formula6 = r'1 + \cat - 2 + \elephant = \frog'
+  #formula7 = r'a + b \cat - c + \elephant = \frog'
+  #formula8 = r'd s _ { 1 1 } ^ { 2 } \frog = \crab d x ^ { + } d x ^ { - } + l _ { p } ^ { 9 } \frac { p _ { - } } { r ^ { 7 } } \delta ( x ^ { - } ) d x ^ { - } d x ^ { - }'
+  #import random
   #formula = random.choice([formula1, formula2, formula3, formula4, formula5, formula6, formula7, formula8])
   example = tokenizer(formula, truncation=True, max_length=1024)
   input_ids = example['input_ids']
@@ -93,10 +98,12 @@ def preprocess_formula(formula):
 
 def transform(examples):
     images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+    gold_images = [image for image in examples["image"]]
     formulas_and_masks = [preprocess_formula(formula) for formula in examples['formula']]
     formulas = [item[0] for item in formulas_and_masks]
     masks = [item[1] for item in formulas_and_masks]
-    return {"images": images, 'input_ids': formulas, 'attention_mask': masks}
+    filenames = examples['filename']
+    return {"images": images, 'input_ids': formulas, 'attention_mask': masks, 'filenames': filenames, 'gold_images': gold_images}
     #return {"images": images}
 
 dataset.set_transform(transform)
@@ -105,13 +112,21 @@ eos_id = tokenizer.encode(tokenizer.eos_token)[0]
 def collate_fn(examples):
     #import pdb; pdb.set_trace()
     max_len = max([len(example['input_ids']) for example in examples]) + 1
+    examples_out = []
     for example in examples:
+        example_out = {}
         orig_len = len(example['input_ids'])
         formula = example['input_ids'] + [eos_id,] * (max_len - orig_len)
-        example['input_ids'] = torch.LongTensor(formula)
+        example_out['input_ids'] = torch.LongTensor(formula)
         attention_mask = example['attention_mask'] + [1,] + [0,] * (max_len - orig_len - 1)
-        example['attention_mask'] = torch.LongTensor(attention_mask)
-    batch = default_collate(examples)
+        example_out['attention_mask'] = torch.LongTensor(attention_mask)
+        example_out['images'] = example['images']
+        examples_out.append(example_out)
+    batch = default_collate(examples_out)
+    filenames = [example['filenames'] for example in examples]
+    gold_images = [example['gold_images'] for example in examples]
+    batch['filenames'] = filenames
+    batch['gold_images'] = gold_images 
     #for k in batch:
     #    v = batch[k]
     #    if k != 'images':
@@ -124,8 +139,8 @@ import random
 random.seed(1234)
 np.random.seed(1234)
 #train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True, collate_fn=collate_fn)
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True, collate_fn=collate_fn, worker_init_fn=np.random.seed(0), num_workers=0)
-eval_dataloader = train_dataloader
+#train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True, collate_fn=collate_fn, worker_init_fn=np.random.seed(0), num_workers=0)
+eval_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.eval_batch_size, shuffle=False, collate_fn=collate_fn, worker_init_fn=np.random.seed(0), num_workers=0)
 
 
 
@@ -158,14 +173,15 @@ model = UNet2DConditionModel(
           mid_block_type='UNetMidBlock2DCrossAttnDecoderPositionEncoderPosition'
     )
 
+model = model.cuda()
 
 
-#t5_encoder = T5ForConditionalGeneration.from_pretrained('t5-small').encoder
-t5_encoder = AutoModel.from_pretrained(model_type).cuda()
-def forward_t5(input_ids, attention_mask):
+#text_encoder = T5ForConditionalGeneration.from_pretrained('text-small').encoder
+text_encoder = AutoModel.from_pretrained(model_type).cuda()
+def forward_text(input_ids, attention_mask):
   with torch.no_grad():
     #import pdb; pdb.set_trace()
-    outputs = t5_encoder(input_ids=input_ids, attention_mask=attention_mask)
+    outputs = text_encoder(input_ids=input_ids, attention_mask=attention_mask)
     last_hidden_state = outputs.last_hidden_state 
     last_hidden_state = attention_mask.unsqueeze(-1) * last_hidden_state
   return last_hidden_state
@@ -174,15 +190,15 @@ def forward_t5(input_ids, attention_mask):
 
 
 noise_scheduler = DDPMScheduler(num_train_timesteps=1000, tensor_format="pt")
-
-optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
-
-
-lr_scheduler = get_cosine_schedule_with_warmup(
-    optimizer=optimizer,
-    num_warmup_steps=config.lr_warmup_steps,
-    num_training_steps=(len(train_dataloader) * config.num_epochs),
-)
+#
+#optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
+#
+#
+#lr_scheduler = get_cosine_schedule_with_warmup(
+#    optimizer=optimizer,
+#    num_warmup_steps=config.lr_warmup_steps,
+#    num_training_steps=(len(train_dataloader) * config.num_epochs),
+#)
 
 
 
@@ -202,18 +218,24 @@ def make_grid(images, rows, cols):
 def evaluate(config, epoch, pipeline):
     # Sample some images from random noise (this is the backward diffusion process).
     # The default pipeline output type is `List[PIL.Image]`
-    for step, batch in enumerate(train_dataloader):
+    # Save the images
+    gold_dir = os.path.join(config.output_dir, "images_gold")
+    os.makedirs(gold_dir, exist_ok=True)
+    pred_dir = os.path.join(config.output_dir, "images_pred")
+    os.makedirs(pred_dir, exist_ok=True)
+    for step, batch in tqdm.tqdm(enumerate(eval_dataloader)):
+        gold_images = batch['gold_images']
+        filenames = batch['filenames']
         clean_images = batch['images']
         input_ids = batch['input_ids'].cuda()
         masks = batch['attention_mask'].cuda()
-        encoder_hidden_states = forward_t5(input_ids, masks)
+        encoder_hidden_states = forward_text(input_ids, masks)
         for iii, input_id in enumerate(input_ids):
             formula = tokenizer.decode(input_id, skip_special_symbols=True).replace('<|endoftext|>', '')
             print (f'{iii:04d}: {formula}')
             print ()
-        swap_step = 800
         swap_step = -1
-        images = pipeline.swap(
+        pred_images = pipeline.swap(
             batch_size = config.eval_batch_size, 
             generator=torch.manual_seed(config.seed),
             encoder_hidden_states = encoder_hidden_states,
@@ -221,96 +243,21 @@ def evaluate(config, epoch, pipeline):
             swap_step=swap_step,
         )["sample"]
 
-    # Make a grid out of the images
-        image_grid = make_grid(images, rows=batch_size, cols=1)
+        for filename, gold_image, pred_image in zip(filenames, gold_images, pred_images):
+            gold_image.save(os.path.join(gold_dir, filename))
+            pred_image.save(os.path.join(pred_dir, filename))
+        break
 
-        # Save the images
-        test_dir = os.path.join(config.output_dir, "samples")
-        os.makedirs(test_dir, exist_ok=True)
-        image_grid.save(f"{test_dir}/swap_{swap_step}.png")
+        # Make a grid out of the images
+        #image_grid = make_grid(images, rows=batch_size, cols=1)
+
+        #image_grid.save(f"{test_dir}/swap_{swap_step}.png")
         print ('='*10)
         break
 
 
 
-def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
-    # Initialize accelerator and tensorboard logging
-    accelerator = Accelerator(
-        mixed_precision=config.mixed_precision,
-        gradient_accumulation_steps=config.gradient_accumulation_steps, 
-        log_with="tensorboard",
-        logging_dir=os.path.join(config.output_dir, "logs")
-    )
-    if accelerator.is_main_process:
-        if config.push_to_hub:
-            repo = init_git_repo(config, at_init=True)
-        accelerator.init_trackers("train_example")
-    
-    # Prepare everything
-    # There is no specific order to remember, you just need to unpack the 
-    # objects in the same order you gave them to the prepare method.
-    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        model, optimizer, train_dataloader, lr_scheduler
-    )
-    
-    global_step = 0
-
-    # Now you train the model
-    for epoch in range(config.num_epochs):
-        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
-        progress_bar.set_description(f"Epoch {epoch}")
-
-        for step, batch in enumerate(train_dataloader):
-            clean_images = batch['images']
-            input_ids = batch['input_ids'].cuda()
-            masks = batch['attention_mask'].cuda()
-            encoder_hidden_states = forward_t5(input_ids, masks)
-            # Sample noise to add to the images
-            noise = torch.randn(clean_images.shape).to(clean_images.device)
-            bs = clean_images.shape[0]
-
-            # Sample a random timestep for each image
-            timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
-
-            # Add noise to the clean images according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
-            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
-            
-            with accelerator.accumulate(model):
-                # Predict the noise residual
-                noise_pred = model(noisy_images, timesteps, encoder_hidden_states)["sample"]
-                loss = F.mse_loss(noise_pred, noise)
-                accelerator.backward(loss)
-
-                accelerator.clip_grad_norm_(model.parameters(), 1.0)
-                optimizer.step()
-                lr_scheduler.step()
-                optimizer.zero_grad()
-            
-            progress_bar.update(1)
-            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
-            progress_bar.set_postfix(**logs)
-            accelerator.log(logs, step=global_step)
-            global_step += 1
-            break
-
-        # After each epoch you optionally sample some demo images with evaluate() and save the model
-        if accelerator.is_main_process:
-            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
-
-            if (epoch + 1) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
-                evaluate(config, epoch, pipeline)
-
-            if (epoch + 1) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
-                if config.push_to_hub:
-                    push_to_hub(config, pipeline, repo, commit_message=f"Epoch {epoch}", blocking=True)
-                else:
-                    pipeline.save_pretrained(config.output_dir) 
-
-args = (config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
-
-#notebook_launcher(train_loop, args, num_processes=1)
-state_dict = torch.load('model_e500_lr0.0001.pt.20', map_location='cpu')
+state_dict = torch.load('model_e500_lr0.0001.pt.45', map_location='cpu')
 state_dict_out = {}
 for k in state_dict:
     k_out = k.replace('module.', '')
@@ -324,10 +271,7 @@ accelerator = Accelerator(
         log_with="tensorboard",
         logging_dir=os.path.join(config.output_dir, "logs")
     )
-model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        model, optimizer, train_dataloader, lr_scheduler
-    )
-pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+pipeline = DDPMPipeline(unet=model, scheduler=noise_scheduler)
 
 evaluate(config, 0, pipeline)
 
