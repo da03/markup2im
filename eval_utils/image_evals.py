685f436ad12719106f7e35de961b3bd1b1f839a4
kojimano <nk654@cornell.edu>
2022-09-22 15:39:06 -0400

    Euclidean distance translation invariant version.

--- eval_utils/image_evals.py
+++ eval_utils/image_evals.py
@@ -31,41 +31,45 @@ def eval_image(pred_img_path: str, gold_img_path: str) -> Dict:
     evals["ssim"] = ssim_val
 
     # other metrics
+    """
     evals["psnr"] = psnr(pred_img_cv2, gold_img_cv2)
     evals["uqi"] = uqi(pred_img_cv2, gold_img_cv2)
     evals["ergas"] = - ergas(pred_img_cv2, gold_img_cv2)
     evals["scc"] = scc(pred_img_cv2, gold_img_cv2)
     evals["rase"] = -rase(pred_img_cv2, gold_img_cv2)
     evals["sam"] = -sam(pred_img_cv2, gold_img_cv2)
-    #evals["msssim"] = msssim(pred_img_cv2, gold_img_cv2)
+    evals["msssim"] = msssim(pred_img_cv2, gold_img_cv2)
     evals["vifp"] = vifp(pred_img_cv2, gold_img_cv2)
+    """
 
     # calculate CLIP distance (↑)   
     evals["clip"] = clip_score(pred_img_pil, gold_img_pil)
 
     # calculate custom metrics
     evals["dtw_euclidean"] = -calc_dtm_score(pred_img_cv2, gold_img_cv2)
+    evals["dtw_euclidean_translation_invariant"] = -calc_dtm_score(pred_img_cv2, gold_img_cv2, "euc_tsinv")
 
     return evals
-    
+
 """
 def eval_image(pred_img_path: str, gold_img_path: str) -> Dict:
     evals = {}
     # load images
     pred_img_cv2, gold_img_cv2 = load_image_cv2(pred_img_path), load_image_cv2(gold_img_path)
     pred_img_pil, gold_img_pil = load_image_pil(pred_img_path), load_image_pil(gold_img_path)
+    img_id = os.path.basename(gold_img_path).replace(".png", "")
 
     # calculate custom metrics
     evals["dtw_euclidean"] = -calc_dtm_score(pred_img_cv2, gold_img_cv2)
+    evals["dtw_euclidean_translation_invariant"] = -calc_dtm_score(pred_img_cv2, gold_img_cv2, "euc_tsinv")
 
     return evals
 """
 
 
-def visualize_ranked_results(sorted_dict: str, met_name: str):
+def visualize_ranked_results(sorted_dict: str, met_name: str, output_folder: str):
     # create visualization folder
-    folder_name = "visualization"
-    os.makedirs(folder_name, exist_ok=True)
+    os.makedirs(output_folder, exist_ok=True)
     n = len(sorted_eval_dicts)
 
     fig, ax = plt.subplots(n, 2, figsize=(8, 1*n))
@@ -82,15 +86,16 @@ def visualize_ranked_results(sorted_dict: str, met_name: str):
         ax[i][1].set_title("{} ".format(score))
         #ax[i][1].set_title("{} gt: {}".format(id, score))
 
-    fig_name = os.path.join(folder_name, f"{met_name}.png")
+    fig_name = os.path.join(output_folder, f"{met_name}.png")
     plt.savefig(fig_name)
     plt.close()
 
 if __name__ == "__main__":
     # testing out image eval functions
-    pred_img_folder = "../images_rendered/images_pred"
+    pred_img_folder =  "../images_rendered/images_pred" # "../images_rendered_html/images_pred"
     pred_img_pathes = [os.path.join(pred_img_folder, f) for f in os.listdir(pred_img_folder) if ".png" in f]
-    gold_img_folder = "../images_rendered/images_gold"
+    gold_img_folder =  "../images_rendered/images_gold" #"../images_rendered_html/images_gold"
+    output_folder = "html_visualization"
     
     results = []
     for pred_img_path in pred_img_pathes:
@@ -102,7 +107,7 @@ if __name__ == "__main__":
         print("{}: {}".format(img_id, eval))
 
     # create ranking according to metrics
-    for met_name in ["rmse", "ssim", "clip", "psnr", "uqi", "ssim", "ergas", "scc", "rase", "sam", "vifp", "dtw_euclidean"]:
+    for met_name in ["rmse", "ssim", "clip", "psnr", "uqi", "ssim", "ergas", "scc", "rase", "sam", "vifp", "dtw_euclidean", "dtw_euclidean_translation_invariant"]:
         eval_dicts = {}
         for r in results:
             if met_name in r:
@@ -110,7 +115,7 @@ if __name__ == "__main__":
         
         if len(eval_dicts) > 0:
             sorted_eval_dicts = {k: v for k, v in sorted(eval_dicts.items(), key=lambda item: item[1], reverse=True)}
-            visualize_ranked_results(sorted_eval_dicts, met_name)
+            visualize_ranked_results(sorted_eval_dicts, met_name, output_folder)
 
     
 
--- eval_utils/metric_utils.py
+++ eval_utils/metric_utils.py
@@ -20,21 +20,103 @@ def get_column_features(img: np.array, th = 150):
     features = []
     binary_img = (img < th).astype(int)
     #cv2.imwrite("img.png", img )
-    #cv2.imwrite("binary.png", binary_img * 255)
+    cv2.imwrite("gt.png", binary_img * 255)
     for c in range(binary_img.shape[1]):
         features.append(binary_img[:, c])
     return features
+
+def euclidean_dist_translation_invariant(x, y):
+    '''
+    allow 10% of vertical translation
+    '''
+    num_rows = x.shape[0]
+    slack = int(np.floor(num_rows * 0.1))
+    dists = []
+
+    # no translation
+    diff = (x - y) ** 2
+    dist = np.sum(diff)
+    dists.append(dist)
+    
+    if dist != 0.:
+        # translate x downward (up to 10%)
+        for s in range(1, slack+1):
+            diff = (x[s:] - y[:-s]) ** 2
+            dist = np.sum(diff)
+            dists.append(dist)
+
+        # translate y downward (up to 10%)
+        for s in range(1, slack+1):
+            diff = (x[:-s] - y[s:]) ** 2
+            dist = np.sum(diff)
+            dists.append(dist)
+
+    return np.min(dists)
+
+from numba import njit, prange
+@njit()
+def euclidean_dist_translation_invariant_faster(x, y):
+    num_rows = x.shape[0]
+    slack = int(np.floor(num_rows * 0.1))
+    dists = []
+
+    # no translation
+    dist = 0.
+    for di in range(x.shape[0]):
+        diff = (x[di] - y[di])
+        dist += diff * diff
+    dists.append(dist)
+
+    if dist != 0.:
+        # translate x downward (up to 10%)
+        for s in range(1, slack+1):
+            dist = 0.
+            for di in range(x.shape[0]):
+                if di+s < num_rows:
+                    diff = (x[di+s] - y[di])
+                    dist += diff * diff
+            dists.append(dist)
+
+        # translate y downward (up to 10%)
+        for s in range(1, slack+1):
+            dist = 0.
+            for di in range(x.shape[0]):
+                if di+s < num_rows:
+                    diff = (x[di] - y[di+s])
+                    dist += diff * diff
+            dists.append(dist)
+
+    return min(dists)
+
+def dot_product(x, y):
+    '''
+    the difference from euclidiean distance is that each feature is normalized.
+    '''
+    cost = 0.
+    return cost
+
+def dot_product_translation_invariant(x, y):
+    cost = 0.
+    return cost
+
+dist_metrics = {
+    "euc_tsinv": euclidean_dist_translation_invariant_faster,
+    "dot": dot_product,
+    "dot_tsinv": dot_product_translation_invariant,
+}
 
 def calc_dtm_score(pred_img, gold_img, dist_metric=None):
     # get column fetures
     pred_feats = get_column_features(pred_img)
     gold_feats = get_column_features(gold_img)
-    
-    # calculate features
+
+    # calculate cost
     if dist_metric is None:
         path, cost = dtw_path(pred_feats, gold_feats)
     else:
-        path, cost = dtw_path_from_metric(pred_feats, gold_feats, dist_metric)
+        path, cost = dtw_path_from_metric(pred_feats, gold_feats, dist_metrics[dist_metric])
+        if "euc" in dist_metric:
+            cost = np.sqrt(cost)
 
     # normalize score between 0 and 1
 
